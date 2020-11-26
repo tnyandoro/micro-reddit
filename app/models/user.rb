9b280205634c04c02f636aad629d67a83390e22f
bini-i <binyam.sh1@gmail.com>
2020-11-26 15:50:46 +0300

    Update User model
    - add validation for uniqueness of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
     validates :email, presence: true
     validates :password, presence: true
     validates :user_name, uniqueness: true
+    validates :email, uniqueness: true
     validates_length_of :user_name, minimum: 6, maximum: 12
     validates_length_of :password, minimum: 8
 end
